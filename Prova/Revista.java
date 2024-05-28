11e57a9793eee59c5c6ea65d3913d46e8fa742cf
João Pedro <147161269+Joao2007Pedro@users.noreply.github.com>
2024-05-28 19:48:49 -0300

    Revista.java
    Herda de Publicacao.
    Atributos adicionais privados: numeroEdicao (int), mesPublicacao (String).
    Métodos:
    Construtor que inicializa todos os atributos, incluindo os da classe base.
    Métodos getters e setters para os novos atributos.
    Sobrescrita do método exibirDetalhes() para incluir o número da edição e o mês de publicação.
    Sobrecarga do método exibirDetalhes() para aceitar um parâmetro booleano que indica se a
    exibição deve incluir ou não o mês de publicação

---
+++ Prova/Revista.java
@@ -0,0 +1,36 @@
+public class Revista extends Publicacao{
+	private int numeroEdicao;
+	private String mesPublicacao;
+	
+	public Revista (String titulo, String autor, int anoPublicacao, int numeroEdicao, String mesPublicacao) {
+		super(titulo, autor, anoPublicacao);
+		this.numeroEdicao = numeroEdicao;
+		this.mesPublicacao = mesPublicacao;
+	}
+	
+	public int getNumeroEdicao() {
+		return numeroEdicao;
+	}
+	public void setNumeroEdicao(int numeroEdicao) {
+		this.numeroEdicao = numeroEdicao;
+	}
+	public String getMesPublicacao() {
+		return mesPublicacao;
+	}
+	public void setMesPublicacao(String mesPublicacao) {
+		this.mesPublicacao = mesPublicacao;
+	}
+	
+	@Override
+	public void exibirDetalhes() {
+		System.out.println(" Edição Número: " + numeroEdicao + " Mês da Publicação: " + mesPublicacao);
+	}
+	public void exibirDetalhes(boolean temMes) {
+		super.exibirDetalhes();
+		System.out.println(" Edição Número: " + numeroEdicao);
+		if (temMes) {
+			System.out.println(" Mês da Publicação: " + mesPublicacao);
+		}
+	
+}
+}
