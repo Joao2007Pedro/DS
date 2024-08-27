0653530758eafd956c240462a973acaace5d5833
João Pedro <147161269+Joao2007Pedro@users.noreply.github.com>
2024-08-27 20:57:46 -0300

    Moto.java
    A classe Moto

--- Classe Abstrata/Exercicio_2/Moto.java
+++ Classe Abstrata/Exercicio_2/Moto.java
@@ -4,7 +4,7 @@ public class Moto extends Veiculo {
     public Moto(String modelo, double valorTabela, String cor, String combustivel) {
         super(modelo, valorTabela, cor, combustivel);
     }
-
+ 
     @Override
     public double calculaIpva() {
         return getValorTabela() * 0.02;
